7e6d00f8eac8d3ebe25754add8819a45aa3dfc96
bowenkuo <thisisobowenkuo@gmail.com>
2021-01-22 16:19:08 +0800

    Add: add.R

---
+++ fakelib/R/add.R
@@ -0,0 +1,11 @@
+#' Add two numbers
+#'
+#' Add two numbers
+#'
+#' @param a first number
+#' @param b second number
+#' @return an added number
+#' @export
+add <- function(a, b) {
+  return(a + b)
+}
